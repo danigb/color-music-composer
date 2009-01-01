5b659f7871386cffd511d6485f183751b0a913d2
Brylie <brylie@ibiblio.org>
2009-01-01 00:55:14 -0600

    Italian dynamic markings.

---
+++ dynamics.rb
@@ -0,0 +1,6 @@
+# This file defines terminology for amplitude modulation.
+abrupt_dynamic_definitions = {"pianissimo" => "Very soft.", "piano" => "Soft.", "mezzo piano" => "Moderately soft.", "mezzo forte" => "Moderately loud.", "forte" => "Loud.", "fortissimo" => "Very Loud.", "tacet" => "Silent", "sforzndo" => "Sudden emphasis.", "marcato" => "Marked, accented", "leggiero" => "Light, delicate.", "espressivo" => "With expression", "brio" => "With vigor.", "fine" => "The end."}
+
+abrupt_dynamic_abbreviations = {"pianissimo" => "pp.", "piano" => "p.", "mezzo piano" => "m.p.", "mezzo forte" => "m.f.", "forte" => "f.", "fortissimo" => "ff.", "sforzando" => "sfz."}
+
+gradual_dynamic_definitions = {"calando" => "Decreasing tone and speed.", "morendo" => "Fading away.", "diminuendo" => "Diminishing.", "incalzando" => "Increasing tone and speed."
