5e5acf150c802f24cab1969492a1891845a53091
Brylie <brylie@ibiblio.org>
2009-01-01 00:27:01 -0600

    Added tempo modifiers.

--- tempo.rb
+++ tempo.rb
@@ -11,3 +11,11 @@ tempo_terms = {"grave" => "Somber, serious, solemn.", "lento" => "Slow, slack.",
 
 # Tempo ranges are approximations of the range of tempos which give a sense of emotion or urgency to a composition.
 tempo_ranges = {"grave" => 40..51, "lento" => 52..63, "adagio" => 64..75, "moderato" => 76..91, "allegro" => 92..115, "vivace" => 116..160, "presto" => 161..208}
+
+=begin
+The following section expresses common tempo change terminology. The dictoinaries can be used for education, sheet music notation and also to create variations in generated musical compositions.
+=end
+# Various tempo modifiers.
+tempo_change_descriptions = {"ritardando" => "Gradually slow down.", "rallentando" => "Gradually slower.", "ritenuto" => "Immediate tempo reduction.", "accelerando" => "Gradually faster.", "doppio movimento" => "Double tempo.", "a tempo" => "Resume prior tempo.", "tempo primo" => "Resume initial tempo"}
+
+# The next step will be to apply functions to the tempo modifiers.
