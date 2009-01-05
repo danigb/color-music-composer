d3e1a22a02471eea4b20a5a6b4de5e5437afdf9f
EvanBurchard <evan.burchard@gmail.com>
2009-01-05 10:16:57 +0800

    added support for more arbitrary intervals...  Should probably be refactored into functions such as 4.d to allow for even more arbitrary stuff.  ex. doubly diminished 4th could be 4.d.d
    Signed-off-by: Brylie <brylie@gnumedia.org>

--- lib/intervals.rb
+++ lib/intervals.rb
@@ -4,32 +4,70 @@
 # The interval designations are as follow: Perfect Unison, minor 2ND, Major 2ND,
 # minor 3RD, Major 3RD, Perfect 4TH, Augmented 4TH, Diminished 5TH, Perfect 5TH,
 # minor 6TH, Major 6TH, minor 7TH, Major 7TH, Perfect Octave.
+
+:dU = -1
 :U = 0
+:AU = 1
+
+:d2 = 0
 :m2 = 1
 :M2 = 2
+:A2 = 3
+
+:d3 = 2
 :m3 = 3
 :M3 = 4
+:A3 = 5
+
+:d4 = 4
 :P4 = 5
-:aug4 = 6
-:dim5 = 6
+:A4 = 6
+
+:d5 = 6
 :P5 = 7
+:A5 = 8
+
+:d6 = 7
 :m6 = 8
 :M6 = 9
+:A6 = 10
+
+:d7 = 9
 :m7 = 10
 :M7 = 11
-:O = 12
+:A7 = 12
+
+:d8 = 11
+:P8 = 12
+:A8 = 13
+
+:d9 = 12
 :m9 = 13
 :M9 = 14
+:A9 = 15
+
+:d10 = 14
 :m10 = 15
 :M10 = 16
+:A10 = 17
+
+:d11 = 16
 :P11 = 17
 :aug11 = 18
+
 :dim12 = 18
 :P12 = 19
+:A12 = 20
+
+:d13 = 19
 :m13 = 20
 :M13 = 21
+:A13 = 22
+
+:d14 = 21
 :m14 = 22
 :M14 = 23
+:A14 = 24
 
 # This deviates a bit from standardized notation for the interval names.
 # Symbols normally used to designate intervals, such as ° and +, might 
